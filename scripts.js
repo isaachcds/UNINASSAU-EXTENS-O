4181bccd6cd64802d7c7937c43342ffe5d19a754
isaachcds <isaachelenohr@gmail.com>
2023-08-27 11:27:31 -0300

    mensagem no código de JS

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
-const nav = document.getElementById('nav');
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 370){ 
-        nav.classList.add('border_black')
+// const nav = document.getElementById('nav');
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY >= 370){ 
+//         nav.classList.add('border_black')
 
-} else {
-    nav.classList.remove('border_black')
-}
-})
+// } else {
+//     nav.classList.remove('border_black')
+// }
+// })
+
+
+//utilizar depois de terminar a estrutura do HTML...
